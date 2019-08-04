d4890b29da900d65069df9f776d4c453559caffd
Aashutosh Rathi <aashutoshrathi@gmail.com>
2019-08-04 11:45:25 +0530

    hotfixes

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,8 @@
 const path = require("path");
+const HDWalletProvider = require("truffle-hdwallet-provider");
+
+var mnemonic =
+  "frost toast island december combine recipe friend bring jungle skin cloth rate";
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -8,6 +12,15 @@ module.exports = {
     develop: {
       port: 8545,
       provider: "https://testnet2.matic.network"
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(
+          mnemonic,
+          "https://ropsten.infura.io/v3/abb9d23bcaa6458e81b2adb42466df00"
+        );
+      },
+      network_id: 3
     }
   }
 };
